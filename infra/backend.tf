bc6460efe1b55426040fbb4433f2b84656c2dd32
Hélio Barros <heliojudson2015@gmail.com>
2026-09-02 22:34:03 -0300

    infra: bootstrap terraform backend (S3 + native lock)

---
+++ infra/backend.tf
@@ -0,0 +1,16 @@
+﻿terraform {
+  required_version = ">= 1.10"
+  required_providers {
+    aws = {
+      source  = "hashicorp/aws"
+      version = "~> 5.0"
+    }
+  }
+  backend "s3" {
+    bucket       = "toggle-master-tfstate-heliobarros-2026"
+    key          = "terraform.tfstate"
+    region       = "us-east-2"
+    encrypt      = true
+    use_lockfile = true
+  }
+}
